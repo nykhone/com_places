c65a9d75f34a44d575a9b1d2ea1a0cfe96f27752
Nykhone <nykhone@yahoo.fr>
2013-02-08 03:20:34 +0700

    add db tool singleton

---
+++ admin/sql/db_tool.php
@@ -0,0 +1,49 @@
+<?php
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+class DB_Tool
+{
+	private static $_instance;
+	private function __construct() {}
+	private function __clone() {}
+
+	public static function get()
+	{
+		if ( !( self::$_instance instanceof self ) )
+		{
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	private function __runSql( $sql_string )
+	{
+		$instr = explode( ' ', trim( $sql_string ) );
+		$com = strtoupper( $instr[0] );
+
+		$aDB = JFactory::getDBO();
+		$aDB->setQuery( $sql_string );
+
+		if ($com == "INSERT" || $com == "DELETE" || $com == "UPDATE" || $com == "REPLACE")
+		{
+			$result = $aDB->query();
+			if ($aDB->getErrorNum())
+			{
+				JError::raiseWarning(500, $aDB->getErrorMsg());
+			}
+		}
+		else
+		{
+//			$result = $database->loadAssocList();
+			$result = $aDB->loadObjectList();
+			if ($aDB->getErrorNum())
+			{
+				JError::raiseWarning(500, $aDB->getErrorMsg());
+			}
+		}
+
+		return $result;
+	}
+
+}
