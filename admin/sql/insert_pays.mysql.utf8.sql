55317cf5b15ad30900ab8674c2b3dc219c1a74d6
Nykhone <nykhone@yahoo.fr>
2013-02-10 04:17:26 +0700

    recupere data from sql.free.fr

--- admin/sql/insert_pays.mysql.utf8.sql
+++ admin/sql/insert_pays.mysql.utf8.sql
@@ -2,30 +2,30 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `joom258_places_pays` (
-  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
-  `nom` varchar(30) NOT NULL DEFAULT '?',
-  `drapeau` varchar(50) NOT NULL DEFAULT '?',
-  PRIMARY KEY (`id`),
+  `id` int(5) unsigned NOT NULL auto_increment,
+  `nom` varchar(30) collate latin1_spanish_ci NOT NULL default '?',
+  `drapeau` varchar(50) collate latin1_spanish_ci NOT NULL default '?',
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
 INSERT INTO `joom258_places_pays` (`id`, `nom`, `drapeau`) VALUES
-(4, 'belgique', 'be.png'),
-(5, 'chine', 'cn.png'),
-(6, 'allemagne', 'de.png'),
-(7, 'espagne', 'es.png'),
-(8, 'finlande', 'fi.png'),
-(9, 'france', 'fr.png'),
-(10, 'hong kong', 'hk.png'),
-(11, 'indonesie', 'id.png'),
-(12, 'italie', 'it.png'),
-(13, 'japon', 'jp.png'),
-(14, 'cambodge', 'kh.png'),
-(15, 'malaysie', 'my.png'),
-(16, 'nouvelle zelande', 'nz.png'),
-(17, 'philippines', 'ph.png'),
-(18, 'russie', 'ru.png'),
-(19, 'singapour', 'sg.png'),
-(20, 'thailande', 'th.png'),
-(21, 'vietnam', 'vn.png');
-
+(1, 'france', 'fr.png'),
+(2, 'entre 2', 'e2.png'),
+(3, 'nouvelle zelande', 'nz.png'),
+(4, 'cambodge', 'kh.png'),
+(5, 'thailande', 'th.png'),
+(6, 'belgique', 'be.png'),
+(7, 'chine', 'cn.png'),
+(8, 'vietnam', 'vn.png'),
+(9, 'indonesie', 'id.png'),
+(10, 'espagne', 'es.png'),
+(11, 'russie', 'ru.png'),
+(12, 'singapour', 'sg.png'),
+(13, 'finlande', 'fi.png'),
+(14, 'allemagne', 'de.png'),
+(15, 'hong kong', 'hk.png'),
+(16, 'italie', 'it.png'),
+(17, 'japon', 'jp.png'),
+(18, 'malaysie', 'my.png'),
+(19, 'philippines', 'ph.png');
